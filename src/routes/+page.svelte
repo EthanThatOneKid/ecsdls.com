721f6f67e637a4b1c9c3f62b95db49e4c0d54e2c
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-03-02 13:57:13 -0800

    Add 5 main sections
    Added sections of the following IDs:
    hero, about-us, partners, speakers, volunteers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,77 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="main">
+	<section class="main__section" id="hero">
+		<div class="main__content">
+			<h2>Hero</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget enim vitae urna commodo
+				auctor. Nunc congue augue id purus dignissim malesuada. Duis tincidunt nisl eu ligula
+				consequat auctor. Donec commodo convallis est, sit amet tincidunt enim varius vitae.
+				Suspendisse lobortis nunc a tincidunt rutrum.
+			</p>
+		</div>
+	</section>
+	<section class="main__section" id="about-us">
+		<div class="main__content">
+			<h2>About us</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget enim vitae urna commodo
+				auctor. Nunc congue augue id purus dignissim malesuada. Duis tincidunt nisl eu ligula
+				consequat auctor. Donec commodo convallis est, sit amet tincidunt enim varius vitae.
+				Suspendisse lobortis nunc a tincidunt rutrum.
+			</p>
+		</div>
+	</section>
+	<section class="main__section" id="partners">
+		<div class="main__content">
+			<h2>Partners</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget enim vitae urna commodo
+				auctor. Nunc congue augue id purus dignissim malesuada. Duis tincidunt nisl eu ligula
+				consequat auctor. Donec commodo convallis est, sit amet tincidunt enim varius vitae.
+				Suspendisse lobortis nunc a tincidunt rutrum.
+			</p>
+		</div>
+	</section>
+	<section class="main__section" id="speakers">
+		<div class="main__content">
+			<h2>Speakers</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget enim vitae urna commodo
+				auctor. Nunc congue augue id purus dignissim malesuada. Duis tincidunt nisl eu ligula
+				consequat auctor. Donec commodo convallis est, sit amet tincidunt enim varius vitae.
+				Suspendisse lobortis nunc a tincidunt rutrum.
+			</p>
+		</div>
+	</section>
+	<section class="main__section" id="volunteers">
+		<div class="main__content">
+			<h2>Volunteers</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget enim vitae urna commodo
+				auctor. Nunc congue augue id purus dignissim malesuada. Duis tincidunt nisl eu ligula
+				consequat auctor. Donec commodo convallis est, sit amet tincidunt enim varius vitae.
+				Suspendisse lobortis nunc a tincidunt rutrum.
+			</p>
+		</div>
+	</section>
+</main>
+
+<style>
+	.main {
+		display: block;
+	}
+
+	.main__section {
+		background-color: #f2f2f2;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		outline: 1px solid black;
+	}
+
+	.main__content p {
+		max-width: 80ch;
+		text-align: center;
+	}
+</style>
